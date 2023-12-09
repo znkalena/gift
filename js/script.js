dc6e56773c50fc570e83bad214dae4f7513dab02
znkalena <elze66@gmail.com>
2023-12-09 17:47:48 +0300

    project without message

---
+++ js/script.js
@@ -0,0 +1,96 @@
+const thumbsSwiper = new Swiper(".gift__swiper--thumbs", {
+    slidesPerView: 6,
+    spaceBetween: 12,
+    freeMode: true,
+    watchSlidesProgress: true,
+    breakpoints:{
+    320:{        
+        spaceBetween: 12,
+        slidesPerView: 5,
+    },
+    1141:{
+        spaceBetween: 16,
+        slidesPerView: 6,        
+    }
+    }
+});
+
+const swiperMain = new Swiper(".gift__swiper--card", {
+    spaceBetween:16,    
+    thumbs: {
+        swiper: thumbsSwiper
+    }
+});
+
+const API_URL ='https://curvy-prong-handle.glitch.me/';
+
+const form = document.querySelector(".form");
+const phoneInputs = form.querySelectorAll(".form__field--phone");
+const formButton = form.querySelector(".form__button");
+const cardInput = document.querySelector(".form__card");
+const cardFrom =document.querySelector(".card__from");
+const cardTo = document.querySelector(".card__to");
+const cardMessage =document.querySelector(".card__message");
+
+const updateCardInput =() => {
+    const activeSlide = document.querySelector(".gift__swiper--card .swiper-slide-active");
+    const cardData = activeSlide.querySelector(".gift__card-image").dataset.card;
+    cardInput.value = cardData;
+}
+updateCardInput();
+swiperMain.on("slideChangeTransitionEnd",updateCardInput)
+
+for (let i = 0;i < phoneInputs.length;i++){
+    const element = phoneInputs[i];
+    const maskOptions = {
+        mask: '+{375}(00)000-00-00'
+    };
+    IMask(element, maskOptions);
+};
+
+const updateSubmitButton =() =>{
+    let isFormFilled = true;
+    
+    for(const field of [...form.elements]){
+        if(field.classList.contains('form__field')){
+            if(!field.value.trim()){
+                isFormFilled = false;                
+                break;
+            }
+        }
+    }
+    formButton.disabled = !isFormFilled;
+};
+
+
+form.addEventListener("input",updateSubmitButton);
+
+form.addEventListener("submit",async(event) => {
+event.preventDefault();
+const formData = new FormData(form);
+const data = Object.fromEntries(formData);
+
+try {
+    const response = await fetch(`${API_URL}/api/gift`,{
+        method:'POST',
+        headers:{
+            "Content-Type": "application/json"
+        },
+        body:JSON.stringify(data),
+    });
+    const result =await response.json();
+    if(response.ok){
+        prompt('Postcard saved successfully',`${location.origin}/card.html?id=${result.id}`);        
+    }
+    else{
+        alert(`There is a mistake sending to server: ${result.error}`)
+    } 
+    form.reset();     
+} catch (error) {
+    console.error(`There is a mistake sending form ${error.message}`);
+    alert('Let try again.Something wrong!')
+}
+});
+
+
+
---
+++ js/scriptCard.js
@@ -0,0 +1,54 @@
+const API_URL ='https://curvy-prong-handle.glitch.me/';
+
+const card = document.querySelector(".card");
+const cardTitle =document.querySelector(".card__title");
+const cardContact = document.querySelector(".card__contact");
+const cardImg = document.querySelector(".card__front-img");
+const cardFrom = document.querySelector(".card__from");
+const cardTo = document.querySelector(".card__to");
+const cardMessage =document.querySelector(".card__message");
+
+
+const rerenderContact =() => {
+    const screenWidth = window.innerWidth;
+    if(screenWidth <= 580){
+        card.after(cardContact);
+    }
+    else{
+        cardTitle.after(cardContact);
+    }
+}
+
+const GetGiftData =async(id) =>{
+try {
+    const response =await fetch(`${API_URL}/api/gift/${id}`);    
+    if(response.ok){
+        return response.json();
+    }
+    else{
+        throw new Error("Postcard wasn't founded")
+    }
+} catch (error) {
+    console.error(error);
+}
+}
+const getIdFromUrl =(id) =>{
+    const params = new URLSearchParams(location.search);    
+    return params.get("id");
+}
+const init =async() =>{
+    rerenderContact();
+    window.addEventListener('resize',rerenderContact);
+    const id = getIdFromUrl();    
+    if(id){
+        const data = await GetGiftData(id);
+        if(data){
+            console.log(data);
+            cardImg.src =`img/${data.card}.png`;
+            cardFrom.textContent = data.sender;
+            cardTo.textContent =data.geter;                       
+            cardMessage.textContent =data.message;
+        }
+    }    
+}
+init();

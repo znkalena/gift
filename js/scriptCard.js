ca44d51c43df7c6b7e65843dda03a2a9e18c45c7
znkalena <elze66@gmail.com>
2023-12-10 15:43:32 +0300

    corrected12

--- js/scriptCard.js
+++ js/scriptCard.js
@@ -43,7 +43,8 @@ const init =async() =>{
     if(id){
         const data = await GetGiftData(id);
         if(data){
-            cardImg.src =`/img/${data.card}.png`;            
+            cardImg.src ='/img/card1.png';
+            console.log(cardImg.src)            
             cardFrom.textContent = data.sender +' '+' phone: '+ data.phoneSet;
             cardTo.textContent =data.geter;
             const formatedData =data.message.replaceAll("\n","<br>");                       
